a431a7b4820317f9df084780ca151110935ae689
Mateus Lisboa <chart010@hotmail.com>
2020-08-06 22:09:55 -0300

    middleware login solve problem

--- src/router.js
+++ src/router.js
@@ -131,7 +131,7 @@ const router = new Router({
 router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
     var session = JSON.parse(localStorage.getItem('vue-session-key'));
-    if (session.token == null) {
+    if (!session) {
       next({
         path: '/login',
         params: {
